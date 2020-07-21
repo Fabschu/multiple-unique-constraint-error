d4c1faf057cc877b8ce6a2a114f76b94c499cf9e
Fabian Schulte <schultefabian@yahoo.de>
2020-07-21 12:49:43 +0200

    cleaned up

--- grails-app/init/multiple/unique/constraint/error/BootStrap.groovy
+++ grails-app/init/multiple/unique/constraint/error/BootStrap.groovy
@@ -3,24 +3,8 @@ import sec.Requestmap
 class BootStrap {
 
     def init = { servletContext ->
-      println 1111111
-
-    try{
       new Requestmap(url: '/test1/**', configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY",name:"Assets", httpMethod:"POST").save(failOnError:true)
-      println 2222222
       new Requestmap(url: '/test1/**', configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY",name:"Assets").save(failOnError:true)
-      println 3333333  
-    }catch(Exception e){
-      println e
-    }
-
-    try{
-      new Requestmap(url: '/test2/**', configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY",name:"Assets").save(failOnError:true)
-      new Requestmap(url: '/test2/**', configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY",name:"Assets", httpMethod:"POST").save(failOnError:true)
-    }catch(Exception e){
-      println e
-    }
-
     }
     def destroy = {
     }
